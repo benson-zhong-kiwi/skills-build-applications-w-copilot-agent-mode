5c9671b6ebfd86a9b7d3e4dc812907d16656e1d8
Benson Zhong <167720411+benson-zhong-kiwi@users.noreply.github.com>
2025-08-21 02:24:50 +0000

    Update ALLOWED_HOSTS to include dynamic CODESPACE_NAME for development

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-j&kuir_6ql+nb@!$=51p%-#sb^n3aj3cni2kj(oe(#w7t8(m+#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}')
 
 
 # Application definition
